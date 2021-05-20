6d69d14ea79e345ce80862ae53af991b7760217f
celine.roure <celine.roure.pro@gmail.com>
2021-05-20 22:17:41 +1000

    update how_many_players and create trivia file

---
+++ how_many_players.rb
@@ -0,0 +1,18 @@
+require "tty-prompt"
+require_relative "./Player"
+
+def how_many_players
+  prompt = TTY::Prompt.new
+  players = []
+
+  choices = %w(1 2 3 4)
+  number_of_players = prompt.select("How many players?", choices).to_i
+  
+  for player_position in 1..number_of_players
+    name = prompt.ask("Enter player #{player_position} name?")
+    player = Player.new(name)
+    players << player
+  end
+
+  return players
+end
---
+++ trivia.rb
@@ -0,0 +1,8 @@
+require_relative "./how_many_players"
+
+def trivia
+  players = how_many_players 
+end
+
+trivia
+
