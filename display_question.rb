2ba2ec6ccde9fb2d7b5c98c0e249912c448a9a67
celine.roure <celine.roure.pro@gmail.com>
2021-05-21 22:01:26 +1000

    create a new feature display_questions

---
+++ display_question.rb
@@ -0,0 +1,15 @@
+require "tty-prompt"
+
+def display_question(question, difficulty, player)
+  prompt = TTY::Prompt.new
+
+  if difficulty == "Easy"
+    answer = prompt.select(question[:label], question[:multi_choice])
+  else
+    answer = prompt.ask(question[:label])
+  end
+    
+  if answer && answer.downcase == question[:answer]
+    player.add_to_total_score(10)
+  end
+end
