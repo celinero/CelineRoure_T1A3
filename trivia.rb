b616784a715030dd2e6cd0e44b43cd99835df626
celine.roure <celine.roure.pro@gmail.com>
2021-05-22 16:26:49 +1000

    add two new features  and update main method to display questions for each player then their score

--- trivia.rb
+++ trivia.rb
@@ -1,26 +1,24 @@
 require_relative "./how_many_players"
 require_relative "./choose_difficulty"
+require_relative "./display_question"
+require_relative "./get_questions"
 
 def trivia
   players = how_many_players 
   difficulty = choose_difficulty
+  questions = get_questions(5)
+
+  players.each do |player|
+    puts "#{player.name}"
+
+    questions.each do |question|
+      display_question(question, difficulty, player)
+    end
+  end
+  
+  players.each do |player|
+    puts "#{player.name}: #{player.total_score} points"
+  end
 end
 
 trivia
-
-
-
-
-# how_many_players -> [player, player]
-
-# choose_difficulty -> 'easy' | 'medium' | 'hard'
-
-# for player in players do
-#   display_questions -> 
-#     for question in selected_questions do
-#       display_question
-#       player.add_to_total_score()
-#     end
-# end
-
-# display leader board
