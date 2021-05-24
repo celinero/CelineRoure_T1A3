ad92135b41c6403791272cf2d253468e044c4e9c
celine.roure <celine.roure.pro@gmail.com>
2021-05-24 22:12:17 +1000

    add greeting method and clean code in Trivia file

--- trivia.rb
+++ trivia.rb
@@ -1,9 +1,17 @@
 require_relative "./how_many_players"
 require_relative "./choose_difficulty"
-require_relative "./display_question"
 require_relative "./get_questions"
+require_relative "./display_question"
+
+require "colorize"
+
+def greeting
+  puts "Welcome to Harry Potter Trivia Game!"
+end
 
 def trivia
+  greeting 
+  
   players = how_many_players 
   difficulty = choose_difficulty
   questions = get_questions(3)
@@ -11,18 +19,17 @@ def trivia
   players.each do |player|
     puts "\n"
     puts "******************************************"
-    puts "Player #{player.name} it's your turn!"
+    puts "Player #{player.name} it's your turn!".colorize(:light_blue)
 
     questions.each do |question|
       display_question(question, difficulty, player)
     end
   end
   
-  new_players = players.sort_by {|player| -player.total_score}.each do |player|
+  players.sort_by {|player| -player.total_score}.each do |player|
     puts "\n"
     puts "*************************************************************"
-    puts "Player #{player.name} has a total score of  #{player.total_score} points"
-  
+    puts "Player #{player.name} has a total score of #{player.total_score} points".colorize(:red)
   end
 end
 
