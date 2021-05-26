79fcaa3ce1eadce66dd4e23cf1e2e1be0691fc67
celine.roure <celine.roure.pro@gmail.com>
2021-05-26 23:33:09 +1000

    update the command_line_args file

--- command_line_args.rb
+++ command_line_args.rb
@@ -3,13 +3,12 @@ if ARGV.length > 0
   ARGV.clear
 
   case flag
-  when "-help"
-    puts "See further documentation in readme"
-    exit
-  when "-info"
-      puts "This program is using Ruby version: #{RUBY_VERSION}"
-  else
-    puts "Invalid Argument, see readme for options"
-    exit
+    when "-help"
+      puts "Welcome to Harry Potter Trivia Game! Please enter the number of players and their names then choose the difficulty. You are ready to participate in a fun game. If you have more questions, please have a look on the Readme file."
+    when "-info"
+        puts "This program is using Ruby version: #{RUBY_VERSION}"
+    else
+      puts "Invalid Argument, see readme for options"
   end
+  exit
 end
