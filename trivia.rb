f0466e1b2e53acc8e461390250ccc4768c333975
celine.roure <celine.roure.pro@gmail.com>
2021-05-26 10:02:28 +1000

    update method greeting with artii and add method bye before app ends + update some display and colors

--- trivia.rb
+++ trivia.rb
@@ -4,9 +4,15 @@ require_relative "./get_questions"
 require_relative "./display_question"
 
 require "colorize"
+require "artii"
 
 def greeting
-  puts "Welcome to Harry Potter Trivia Game!"
+  puts "\n"
+  a = Artii::Base.new :font => 'slant'
+  puts a.asciify('Welcome to').colorize(:yellow)
+  puts a.asciify('Harry Potter').colorize(:cyan)
+  puts a.asciify('Trivia Game !').colorize(:yellow)
+  puts "\n"
 end
 
 def trivia
@@ -14,23 +20,29 @@ def trivia
   
   players = how_many_players 
   difficulty = choose_difficulty
-  questions = get_questions(3)
+  questions = get_questions(5)
 
   players.each do |player|
     puts "\n"
-    puts "******************************************"
-    puts "Player #{player.name} it's your turn!".colorize(:light_blue)
+    puts "*******************************************************************".colorize(:cyan)
+    puts "Player #{player.name} it's your turn!".colorize(:cyan)
+    puts "\n"
 
     questions.each do |question|
       display_question(question, difficulty, player)
-    end
+    end 
   end
   
   players.sort_by {|player| -player.total_score}.each do |player|
     puts "\n"
-    puts "*************************************************************"
-    puts "Player #{player.name} has a total score of #{player.total_score} points".colorize(:red)
+    puts "******************************************"
+    puts "Player #{player.name} has a total score of #{player.total_score} points".colorize(:cyan)
   end
+  def bye
+    puts "\n"
+    puts "I hope you had fun, see you soon!".colorize(:yellow)
+  end
+  bye
 end
 
 trivia
